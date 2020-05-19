fbc4e123f428dc69e3003c2f364b3348a3632220
Stephen Castaneda <stephen.castaneda40@gmail.com>
2020-05-18 19:11:35 -0500

    login works

--- src/App/App.js
+++ src/App/App.js
@@ -1,18 +1,52 @@
 import React from 'react';
 import './App.scss';
 
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+import fbConnection from '../helpers/data/connection';
+
 import Auth from '../components/Auth/Auth';
 import BoardContainer from '../components/BoardContainer/BoardContainer';
 import MyNavbar from '../components/MyNavbar/MyNavbar';
 
+fbConnection();
+
 class App extends React.Component {
+  state = {
+    authed: false,
+  }
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
   render() {
+    const { authed } = this.state;
+    const loadComponent = () => {
+      let componentToLoad = '';
+      if (authed) {
+        componentToLoad = <BoardContainer />;
+      } else {
+        componentToLoad = <Auth />;
+      }
+      return componentToLoad;
+    };
     return (
       <div className="App">
         <h2>REACT PINTEREST</h2>
         <MyNavbar />
-        <Auth />
-        <BoardContainer />
+        {loadComponent()}
       </div>
     );
   }
